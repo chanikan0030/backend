a6100cf8196beb3ecd368b9d3f660045e5c122e7
Chanikan <your-email@gmail.com>
2026-03-13 11:50:01 +0700

    1

--- config/pg.js
+++ config/pg.js
@@ -1,12 +1,10 @@
 import pg from "pg";
 const { Pool } = pg;
+import "dotenv/config";
 
 const pool = new Pool({
-  user: "postgres",
-  host: "localhost",
-  database: "chanikan",
-  password: "12345678",
-  port: 5432,
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
 });
 
 export default pool;
